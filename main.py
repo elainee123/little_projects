061a828cddbd49173f5dd988c9c01576d03b48cc
Elaine Zhu <elazhu22@gmail.com>
2026-09-07 00:46:47 -0700

    start of tic tac toe

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
-
+# Series of small games user can choose from
 def main():
-    pass
+    pick_option = input("Pick an Option: ")
+    return pick_option
+
+def tic_tac_toe():
+    board = [[0, 0, 0] for _ in range(3)]
+    print_board(board)
+    win = False
+    while not win:
+        choice = input("Where would you like to input your piece?")
+        board = input_piece(board)
+        won = detect_win(board)
+        if won == "True":
+            win = True
+        elif won == "lost":
+            return False
+    return True
+
+def print_board(b):
+    for row in b:
+        for column in row:
+            print(column + " ", end = "")
+        print()
+    return 
+
+def input_piece(b):
+
+    print_board(b)
+    return b
+
+def detect_win(b):
+    return bool
+    
+# true = win, false = lose
 if __name__ == "__main__":
-    main()
+    option = main()
+    if option == "a":
+        tic_tac_toe()
